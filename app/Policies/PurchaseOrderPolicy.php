f0f3c0280c457808774f4fee5db2c00f3071a0e2
EtzahuMendoza <etzahumendoza@gmail.com>
2025-02-05 07:55:44 -0600

    .

--- app/Policies/PurchaseOrderPolicy.php
+++ app/Policies/PurchaseOrderPolicy.php
@@ -55,7 +55,14 @@ class PurchaseOrderPolicy
      */
     public function update(User $user, PurchaseOrder $purchaseOrder): bool
     {
-        return $user->can('update_purchase::order::purchaser') && $purchaseOrder->status ==='borrador';
+        $states = [
+            'borrador',
+            'devuelto por gerente de compras',
+            'devuelto por gerente solicitante',
+            'devuelto por DG nivel 1',
+            'devuelto por DG nivel 2',
+        ];
+        return $user->can('update_purchase::order::purchaser') &&  in_array($purchaseOrder->status, $states);
     }
 
     /**
