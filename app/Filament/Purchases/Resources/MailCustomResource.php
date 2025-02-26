d8f8e1a34090890c6129ab5f0009cad5a21227ee
EtzahuMendoza <etzahumendoza@gmail.com>
2025-02-26 09:12:24 -0600

    .

--- app/Filament/Purchases/Resources/MailCustomResource.php
+++ app/Filament/Purchases/Resources/MailCustomResource.php
@@ -10,7 +10,11 @@ class MailCustomResource extends MailResource
     // Si comento estas lineas no sale el error Call to a member function keyBy() on null
     public static function canAccess(): bool
     {
-        return  auth()->user()->hasRole('super_admin');
+        if (config('app.env') === 'local' && config('app.debug') === true) {
+            return true;
+        } else {
+            return  auth()->user()->hasRole('super_admin');
+        }
     }
 
     protected static bool $shouldRegisterNavigation = true;
