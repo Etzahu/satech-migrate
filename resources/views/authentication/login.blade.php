036169720dc5621e8394b1467c9ad7012d535948
EtzahuMendoza <etzahumendoza@gmail.com>
2025-04-16 10:23:23 -0600

    .

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -26,19 +26,18 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="login-card">
+                    <div class="login-card" >
                         <div class="shadow-lg theme-form login-form">
                             <div class="d-flex justify-content-center">
-                                <img src="{{ asset('assets/images/logo/logo_GPT.svg') }}" alt="Logo de la empresa" class="img-fluid">
+                                <img src="{{ asset('images/logo-app.png') }}" alt="Logo de la empresa" class="img-fluid">
                             </div>
-                            <h1 class="text-center f-w-600 font-warning">Sa-Tech</h1>
-                            <h2 class="text-center f-w-600 font-primary">¡BIENVENID@!</h2>
+                            <h2 class="mt-4 text-center f-w-600 font-primary">¡BIENVENID@!</h2>
                             <div class="login-social-title">
                                 <h5>
                                     Si eres colaborador:
                                 </h5>
                             </div>
-                            <h6>Para continuar inicia sesión con tu cuenta de GPT SERVICES.</h6>
+                            <h6>Para continuar inicia sesión con tu cuenta @gptservices.com</h6>
                             <div class="form-group">
                                 <a href="{{ route('login.redirect') }}" class="btn btn-primary">
                                     Inicia sesión con Google
