573c17fe7e5d5cbdd9b27e163eeaaa7c8ba7acaa
EtzahuMendoza <etzahumendoza@gmail.com>
2025-05-03 19:36:17 -0600

    .

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    server: {
-        host: '127.0.0.1',  // Add this to force IPv4 only
-    },
     plugins: [
         laravel({
             input: [
